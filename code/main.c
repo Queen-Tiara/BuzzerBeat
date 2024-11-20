b33dc0aac279495524b047ea83490030e4d2f6d0
Queen-Tiara <115942547+Queen-Tiara@users.noreply.github.com>
2024-11-20 04:30:50 -0600

    Finished basic reading and linked lists of backend
    Finished back end (mostly, I want to attempt a js frontend tonight)
    basic file reading and set up is finished, created linked lists to store all of the info for questions and answers and prompts

--- code/main.c
+++ code/main.c
@@ -20,16 +20,21 @@ struct team{
 
 struct question{
     struct answer ans;
+    struct answer pr;
     char store[2000];
     int questNum;
     struct bonusQuestion b;
-    struct bonusQuestion *head;
     struct question *next, *prev;
 
 };
 
 struct bonusQuestion{
-    struct answer bAns;
+    struct answer one_Ans;
+    struct answer two_Ans;
+    struct answer three_Ans;
+    struct answer one_Pr;
+    struct answer two_Pr;
+    struct answer three_Pr;
     char one[2000], two[2000], three[2000];
 };
 
@@ -39,9 +44,9 @@ struct answer{
 };
 
 //Functions
-void clearList(struct answer *head);
-void newQuestion(struct question *head, FILE *file, int lineNum,  FILE *bfile, FILE *afile, FILE *bafile);
-void findQuestNum(FILE *file);
+void clearList(struct question *head);
+void newQuestion(struct question *head, FILE *file,  FILE *bfile, FILE *afile, FILE *bafile);
+void setQuestion(FILE *file, struct question *temp);
 
 int main() {
     //I need to introduce the rules and set up here
@@ -94,28 +99,24 @@ int main() {
     FILE *afile = fopen(answerFile, "r");
     FILE *bafile = fopen(bAnswerFile, "r");
     struct question *head;
-    int lineNum = 1;
-    int readLine = 0;
     if (qfile == NULL) {
         printf("Error opening file!\n");
         return 1;
     }
-    char c;
-    while((c = fgetc(qfile)) != EOF){
-        if (c == '(') {
-            qNum = fgetc(qfile) - '0';
-            
-        }
-        if (c == '\n') {
-            lineNum++;
-            newQuestion(head, qfile, lineNum, bfile, afile, bafile);
-             
-                   
-        }
+    for(int i = 0; i < 20; i++){
+        qNum++;
+        newQuestion(head, qfile, bfile, afile, bafile);
 
     }
 
-    
+    /*Onto gameplay
+    Need timer
+    need to grade answers
+    */
+    bool gameOn = true;
+    while(gameOn){
+
+    }
     
 
     fclose(qfile);
@@ -126,27 +127,36 @@ int main() {
     return 0;
 }
 
-void newQuestion(struct question *head, FILE *qfile, int lineNum, FILE *bfile, FILE *afile, FILE *bafile){
+void newQuestion(struct question *head, FILE *qfile, FILE *bfile, FILE *afile, FILE *bafile){
 
     struct question *temp = head;
+    struct question *t = NULL;
     struct question *newNode = malloc(sizeof(struct question));
     if (temp == NULL) {
          head = newNode;
          newNode->prev = NULL;
+         setQuestion(qfile, head);
+         setAnswers(afile, head);
+         setBonus(bfile, bafile, head);
          return;
         }
     while(temp->next != NULL){
+        t = temp;
         temp = temp->next;
+        temp->prev = t;
     }
     temp->next = newNode;
-    readQuestion(lineNum, qfile, temp->next);
+    head->prev = newNode;
+    setQuestion(qfile, temp->next);
+    setAnswers(afile, temp->next);
+    setBonus(bfile, bafile, temp->next);
     temp->next->questNum = qNum;
-
+    temp->next->next = head;
     return;
 }
 
-void readQuestion(int lineNum, FILE *file, struct question *temp){
-    int targetLine = lineNum; 
+void setQuestion( FILE *file, struct question *temp){
+    int targetLine = 2*qNum; 
     int currentLine = 1;
     char c;
     while((c = fgetc(file)) != EOF){
@@ -164,21 +174,244 @@ void readQuestion(int lineNum, FILE *file, struct question *temp){
     }
 }
 
-void findQuestNum(FILE *file){
+void setAnswers(FILE *file, struct question *temp){
+    int targetLine = qNum*2;
+    int currentLine = 1;
     char c;
-    while ((c = fgetc(file)) != EOF) {
-        if (c == '(') {
-            qNum = fgetc(file) - '0';
-            return;
+    
+    struct answer *a = &(temp->ans); 
+    a = malloc(sizeof(struct answer));
+    struct answer *tempA = NULL;
+    a->prev = NULL;
+    struct answer *p = &(temp->pr); 
+    p = malloc(sizeof(struct answer));
+    struct answer *tempP = NULL;
+    p->prev = NULL;
+    while((c = fgetc(file)) != EOF){
+        if (c == '\n') {
+            currentLine++;     
+        }
+        if (currentLine == targetLine) {
+            int x = 0;
+            int y = 0;
+            while((c = fgetc(file)) != ':' || (c = fgetc(file)) != '\n'){
+                if(c == '|'){ 
+                    tempA = a;
+                    a = a->next;
+                    a->prev = tempA;
+                    a = malloc(sizeof(struct answer));
+                    temp->ans.prev = a;
+                    a->next = temp->ans.prev;
+                    continue;
+                } 
+                
+                a->store[x] = c;
+                x++;
+                
+            }
+            a->store[x] = '\0';
+            if(c == ':'){
+                c = fgetc(file);
+            }
+            while((c = fgetc(file)) != '\n'){
+                if(c == ':'){ 
+                    tempP = p;
+                    p = p->next;
+                    p->prev = tempP;
+                    p = malloc(sizeof(struct answer));
+                    temp->pr.prev = p;
+                    p->next = temp->pr.prev;
+                    continue;
+                }
+                p->store[y] = c;
+                y++;
+            }
+            p->store[y] = '\0';
+            
         }
     }
+    return;
 }
 
-void clearList(struct answer *head) {
-    struct answer* current = head;
-    struct answer* next;
+void setBonus(FILE *file, FILE *afile, struct question *temp){
+    int targetLine = qNum*2;
+    int currentLine = 1;
+    char c;
+    while((c = fgetc(file)) != EOF){
+        if (c == '\n') {
+            currentLine++;     
+        }
+        if (currentLine == targetLine) {
+            int x = 0;
+            while((c = fgetc(file)) != '\n'){
+                temp->b.one[x] = c;
+                x++;
+            }
+            temp->b.one[x] = '\0';
+        }
+        if (currentLine == (targetLine + 1)) {
+            int x = 0;
+            while((c = fgetc(file)) != '\n'){
+                temp->b.two[x] = c;
+                x++;
+            }
+            temp->b.two[x] = '\0';
+        }
+        if (currentLine == (targetLine + 2)) {
+            int x = 0;
+            while((c = fgetc(file)) != '\n'){
+                temp->b.three[x] = c;
+                x++;
+            }
+            temp->b.three[x] = '\0';
+        }
+    }
+    struct answer *a1 = &(temp->b.one_Ans);
+    struct answer *a2 = &(temp->b.two_Ans);
+    struct answer *a3 = &(temp->b.three_Ans);
+    a1 = malloc(sizeof(struct answer));
+    a2 = malloc(sizeof(struct answer));
+    a3 = malloc(sizeof(struct answer));
+    struct answer *tempA = NULL;
+    a1->prev = NULL;
+    a2->prev = NULL;
+    a3->prev = NULL;
+    struct answer *p1 = &(temp->pr); 
+    struct answer *p2 = &(temp->pr); 
+    struct answer *p3 = &(temp->pr); 
+    p1 = malloc(sizeof(struct answer));
+    p2 = malloc(sizeof(struct answer));
+    p3 = malloc(sizeof(struct answer));
+    struct answer *tempP = NULL;
+    p1->prev = NULL;
+    p2->prev = NULL;
+    p3->prev = NULL;
+    while((c = fgetc(afile)) != EOF){
+        if (c == '\n') {
+            currentLine++;     
+        }
+        if (currentLine == targetLine) {
+            int x = 0;
+            while((c = fgetc(afile)) != ':' || (c = fgetc(afile)) != '\n'){
+                if(c == '|'){ 
+                    tempA = a1;
+                    a1 = a1->next;
+                    a1->prev = tempA;
+                    a1 = malloc(sizeof(struct answer));
+                    temp->b.one_Ans.prev = a1;
+                    continue;
+                }
+                a1->store[x] = c;
+                x++;
+            }
+            a1->store[x] = '\0';
+            int y = 0;
+            if(c == ':'){
+                c = fgetc(afile);
+            }
+            while((c = fgetc(afile)) != '\n'){
+                if(c == ':'){ 
+                    tempP = p1;
+                    p1 = p1->next;
+                    p1->prev = tempP;
+                    p1 = malloc(sizeof(struct answer));
+                    temp->pr.prev = p1;
+                    p1->next = temp->pr.prev;
+                    continue;
+                }
+                p1->store[y] = c;
+                y++;
+            }
+            p1->store[y] = '\0';
+        }
+        if (currentLine == (targetLine + 1)) {
+            int x = 0;
+            while((c = fgetc(afile)) != ':' || (c = fgetc(afile)) != '\n'){
+                if(c == '|'){ 
+                    tempA = a2;
+                    a2 = a2->next;
+                    a2->prev = tempA;
+                    a2 = malloc(sizeof(struct answer));
+                    temp->b.two_Ans.prev = a2;
+                    continue;
+                }
+                a2->store[x] = c;
+                x++;
+            }
+            a2->store[x] = '\0';
+            if(c == ':'){
+                c = fgetc(afile);
+            }
+            int y = 0;
+            while((c = fgetc(afile)) != '\n'){
+                if(c == ':'){ 
+                    tempP = p2;
+                    p2 = p2->next;
+                    p2->prev = tempP;
+                    p2 = malloc(sizeof(struct answer));
+                    temp->pr.prev = p2;
+                    p2->next = temp->pr.prev;
+                    continue;
+                }
+                p2->store[y] = c;
+                y++;
+            }
+            p2->store[y] = '\0';
+        }
+        if (currentLine == (targetLine + 2)) {
+            int x = 0;
+            while((c = fgetc(afile)) != ':' || (c = fgetc(afile)) != '\n'){
+                if(c == '|'){ 
+                    tempA = a3;
+                    a3 = a3->next;
+                    a3->prev = tempA;
+                    a3 = malloc(sizeof(struct answer));
+                    temp->b.three_Ans.prev = a3;
+                    continue;
+                }
+                a3->store[x] = c;
+                x++;
+            }
+            a3->store[x] = '\0';
+            if(c == ':'){
+                c = fgetc(afile);
+            }
+            int y = 0;
+            while((c = fgetc(afile)) != '\n'){
+                if(c == ':'){ 
+                    tempP = p3;
+                    p3 = p3->next;
+                    p3->prev = tempP;
+                    p3 = malloc(sizeof(struct answer));
+                    temp->pr.prev = p3;
+                    p3->next = temp->pr.prev;
+                    continue;
+                }
+                p3->store[y] = c;
+                y++;
+            }
+            p3->store[y] = '\0';
+        }
+    }
+    return;
+}
+
+
+void clearList(struct question *head) {
+    struct question* current = head;
+    struct question* next;
 
     while (current != NULL) {
+        
+        free(&(current->ans));
+        free(&(current->pr));
+        free(&(current->b.one_Ans));
+        free(&(current->b.two_Ans));
+        free(&(current->b.three_Ans));
+        free(&(current->b.one_Pr));
+        free(&(current->b.two_Pr));
+        free(&(current->b.three_Pr));
+
         next = current->next;
         free(current);
         current = next;
